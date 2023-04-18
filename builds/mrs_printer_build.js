dd2cb05787a7383cf5591da3c9a6c06c2506a6aa
James T Snell <doc@dawning.ca>
2023-04-18 00:42:13 -0600

    Attempting to hook-up github actions to do some auto builds

---
+++ builds/mrs_printer_build.js
@@ -0,0 +1,38 @@
+module.exports = {
+    //board_env: "STM32F103RE_creality",
+    active: true,
+    meta: {
+        stable_name: "mrsprinter-e3ez-{{marlin_version}}-{{uid}}",
+        nightly_name: "mrsprinter-e3ez-{{current_date}}-{{uid}}"
+    },
+    based_on: {
+        repo: "https://github.com/MarlinFirmware/Configurations/",
+        //path: "/config/examples/Creality/Ender-3/CrealityV422/",
+        path: "/config/examples/Creality/Ender-3%20Pro/BigTreeTech%20SKR%20Mini%20E3%203.0/",
+        stable_branch: "release-{{marlin_version}}",
+        nightly_branch: "bugfix-2.1.x"
+    },
+    configuration: {
+        enable: [
+            ["MOTHERBOARD", "BOARD_BTT_MANTA_E3_EZ_V1_0"],
+            ["X_DRIVER_TYPE",  "TMC2209"],
+            ["Y_DRIVER_TYPE",  "TMC2209"],
+            ["Z_DRIVER_TYPE",  "TMC2209"],
+            ["Z2_DRIVER_TYPE",  "TMC2209"],
+            ["E0_DRIVER_TYPE",  "TMC2209"],
+            //standard leveling menu helper
+            "LCD_BED_TRAMMING",
+            "BED_TRAMMING_INCLUDE_CENTER",
+            ["STRING_CONFIG_H_AUTHOR", "(James Snell)"],
+            ["CUSTOM_MACHINE_NAME", "Mrs. Printer"]
+        ],
+        disable: []
+    },
+    configuration_adv: {
+        enable: [
+            //octoprint
+            "HOST_ACTION_COMMANDS"
+        ],
+        disable: []
+    }
+};
